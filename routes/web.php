5be0a40ca122aa197137fd0ffeaecb5dfec36b8c
Floz8 <55977382+Floz8@users.noreply.github.com>
2022-05-17 14:47:38 +0200

    Ajout du login

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/list', function () {
-    
-});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
